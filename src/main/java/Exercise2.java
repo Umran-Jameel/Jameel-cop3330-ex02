ea8885493ced60ef7c7445137195daa8b977f382
Umran Jameel <umranrazack02@mak-bwk-lmran.local>
2021-06-05 18:59:07 -0400

    Initial commit

---
+++ src/main/java/Exercise2.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Umran Jameel
+ */
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args)
+    {
+        System.out.print("What is the input string? ");
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        int len = str.length();
+        System.out.printf("%s has %d characters.", str, len);
+    }
+}
